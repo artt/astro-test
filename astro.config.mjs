c6149149d54bd46a1ba4d3d50d4eb9a8c92a3975
artt <me@artt.dev>
2024-07-25 23:51:57 +0700

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://artt-astro-test.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), mdx()]
 });
