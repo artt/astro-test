60e17f03b567089470f03405224966992196858c
artt <me@artt.dev>
2024-07-28 21:35:29 +0700

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
+
+const locales = {th: "th-TH", en: "en-US"}
+const defaultLocale = "th"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://artt-astro-test.netlify.app",
-  integrations: [preact(), mdx()]
+  trailingSlash: "always",
+  integrations: [
+    preact(),
+    mdx(),
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ]
 });
